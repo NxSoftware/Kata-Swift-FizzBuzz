ff876b2eb0efa8dba834429c8e218e906f464b8e
Steve Wilford <steve@stevewilford.co.uk>
2017-01-01 18:48:32 +0000

    Remove duplicate %3 an %5 checks

--- Sources/FizzBuzz.swift
+++ Sources/FizzBuzz.swift
@@ -1,15 +1,14 @@
 enum FizzBuzz {
 
   static func value(for int: Int) -> String {
-    if int % 3 == 0 && int % 5 == 0 {
-      return "FizzBuzz"
-    } else if int % 3 == 0 {
-      return "Fizz"
-    } else if int % 5 == 0 {
-      return "Buzz"
-    } else {
-      return String(int)
+    var result = ""
+    if int % 3 == 0 {
+      result += "Fizz"
     }
+    if int % 5 == 0 {
+      result += "Buzz"
+    }
+    return result.isEmpty ? String(int) : result
   }
 
 }
