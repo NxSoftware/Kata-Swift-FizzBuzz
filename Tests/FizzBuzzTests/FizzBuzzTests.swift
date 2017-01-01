e10634ff4d96e80ef764f8571a73149b5a96a17c
Steve Wilford <steve@stevewilford.co.uk>
2017-01-01 18:38:44 +0000

    Extract assertions

--- Tests/FizzBuzzTests/FizzBuzzTests.swift
+++ Tests/FizzBuzzTests/FizzBuzzTests.swift
@@ -4,11 +4,17 @@ import XCTest
 class FizzBuzzTests: XCTestCase {
   
   func test1_gives1() {
-    XCTAssertEqual("1", FizzBuzz.value(for: 1), "1 should be 1")
+    expect(1, toBe: "1", "1 should be 1")
   }
   
   func test2_gives2() {
-    XCTAssertEqual("2", FizzBuzz.value(for: 2), "2 should be 2")
+    expect(2, toBe: "2", "2 should be 2")
+  }
+  
+  // MARK: - Assertions
+  
+  private func expect(_ number: Int, toBe expected: String, _ failureMessage: String, file: StaticString = #file, line: UInt = #line) {
+    XCTAssertEqual(expected, FizzBuzz.value(for: number), failureMessage, file: file, line: line)
   }
   
 }
