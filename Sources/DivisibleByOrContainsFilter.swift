347bccd25989ed423f8dbbab83b452dc49e6a08e
Steve Wilford <steve@stevewilford.co.uk>
2017-01-02 16:06:26 +0000

    Extract integer contains number check

--- Sources/DivisibleByOrContainsFilter.swift
+++ Sources/DivisibleByOrContainsFilter.swift
@@ -1,18 +1,24 @@
 class DivisibleByOrContainsFilter: DivisibleByFilter {
   
   override func decorate(result: String, withValueFor int: Int) -> String {
-    var i = int
-    while i > 0 {
-      if i % 10 == number {
-        return string
-      }
-      i /= 10
-    }
-    if result.isEmpty {
+    if integer(int, contains: number) {
+      return string
+    } else if result.isEmpty {
       return super.decorate(result: result, withValueFor: int)
     } else {
       return result
     }
+  }
+  
+  private func integer(_ int: Int, contains number: Int) -> Bool {
+    var i = int
+    while i > 0 {
+      if i % 10 == number {
+        return true
+      }
+      i /= 10
+    }
+    return false
   }
   
 }
